b761527a901a1b02293df44e7c26511107a9deea
mcapurri <m.capurri@gmail.com>
2021-06-25 15:17:49 +0200

    styling Game Over Screen

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
 
-export default function App() {
+const fetchFonts = () => {
+    return Font.loadAsync({
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    });
+};
+const App = () => {
     const [userNumber, setUserNumber] = useState();
     const [guessRounds, setGuessRounds] = useState(0);
+    const [dataLoaded, setDataLoaded] = useState(false);
 
+    if (!dataLoaded) {
+        return (
+            <AppLoading
+                startAsync={fetchFonts}
+                onFinish={() => setDataLoaded(true)}
+                onError={(err) => console.log(err)}
+            />
+        );
+    }
     const configureNewGameHandler = () => {
         setGuessRounds(0);
         setUserNumber(null);
@@ -42,10 +60,12 @@ export default function App() {
             {content}
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
     },
 });
+
+export default App;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import {
     View,
-    Text,
     StyleSheet,
     Button,
     TouchableWithoutFeedback,
@@ -11,6 +10,8 @@ import {
 
 import Card from '../components/Card';
 import Input from '../components/Input';
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
 import NumberContainer from '../components/NumberContainer';
 import Colors from '../constants/colors';
 
@@ -53,7 +54,7 @@ const StartGameScreen = (props) => {
     if (confirmed) {
         confirmedOutput = (
             <Card style={styles.summaryContainer}>
-                <Text>You selected</Text>
+                <BodyText>You selected</BodyText>
                 <NumberContainer>{selectedNumber}</NumberContainer>
                 <Button
                     title="START GAME"
@@ -71,9 +72,9 @@ const StartGameScreen = (props) => {
             }}
         >
             <View style={styles.screen}>
-                <Text style={styles.title}>Start a New Game!</Text>
+                <TitleText style={styles.title}>Start a New Game!</TitleText>
                 <Card style={styles.inputContainer}>
-                    <Text>Select a Number</Text>
+                    <BodyText style={styles.text}>Select a Number</BodyText>
                     <Input
                         style={styles.input}
                         blurOnSubmit
@@ -116,6 +117,7 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         marginVertical: 10,
+        fontFamily: 'open-sans-bold',
     },
     inputContainer: {
         width: 300,
@@ -138,6 +140,9 @@ const styles = StyleSheet.create({
     summaryContainer: {
         marginTop: 20,
         alignItems: 'center',
+    },
+    text: {
+        fontFamily: 'open-sans',
     },
 });
 
